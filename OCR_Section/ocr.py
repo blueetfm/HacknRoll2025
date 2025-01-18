066bd94b5731071123b2f53c976ee65a78c605ce
ZongZheJiang <jiangzongzhe374@gmail.com>
2025-01-18 18:47:53 +0800

    refined OCR output

--- OCR_Section/ocr.py
+++ OCR_Section/ocr.py
@@ -7,6 +7,23 @@ script_dir = os.path.dirname(__file__)
 rel_path = "src/Resume.jpg"
 text = pytesseract.image_to_string(Image.open(os.path.join(script_dir, rel_path)))
 
-for line in text.split("\n"):
-    print(line)
+split_text = text.split("\n")
+length = len(split_text)
+
+clean_text = []
+temp = []
+
+for i in range(length):
+    if len(split_text[i]) > 0:
+        temp.append(split_text[i])
+    else:
+        joined_string = ' '.join(temp)
+        clean_text.append(joined_string)
+        temp.clear()
+
+
+
+
+
+
 
